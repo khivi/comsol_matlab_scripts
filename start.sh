cbc78213f31f3a02fd07b3513d40bcc720a17552
khivi <khivi@users.noreply.github.com>
2020-08-19 17:27:06 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-  # Copyright (C) 2002 Amit (khivi) Khivesara
+  # Copyright (C) 2020 Amit (khivi) Khivesara
   # This program is free software: you can redistribute it and/or modify
   # it under the terms of the GNU General Public License as published by
   # the Free Software Foundation, either version 3 of the License, or
