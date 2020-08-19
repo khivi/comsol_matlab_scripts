f112c2449d6d0ef954de206b05f42a6a429bb211
khivi <khivi@users.noreply.github.com>
2020-08-19 17:25:45 -0400

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+  # Copyright (C) 2002 Amit (khivi) Khivesara
+  # This program is free software: you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation, either version 3 of the License, or
+  # (at your option) any later version.
+
+  # This program is distributed in the hope that it will be useful,
+  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  # GNU General Public License for more details.
+
+  # You should have received a copy of the GNU General Public License
+  # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+output=`mktemp`
+
+comsol mphserver -np 32 > $output &
+
+RE="(?<=port )\d+"
+grep -q -P "$RE"  <(tail -f $output)
+PORT=$(grep -m 1 -P -o "$RE" <(cat $output))
+
+matlab -nodisplay -singleCompThread -r "addpath('/usr/local/comsol55/multiphysics/mli'); mphstart('localhost', $PORT); dust_coupledC_thet(*); exit"
