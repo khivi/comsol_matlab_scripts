4547ba1f7b1eb0e8f3b87f12346d5c086d8fae8b
khivi <work@khivi.com>
2020-09-22 14:49:00 -0400

    Script works with shell

--- abaqus_wait.sh
+++ abaqus_wait.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 JOB=$1
 
 
 wait_for_abaqus() {
   file="$1.sta"
-  grep  --quiet --line-regexp --fixed-strings --regexp=' THE ANALYSIS HAS NOT BEEN COMPLETED' --regexp=' THE ANALYSIS HAS COMPLETED SUCCESSFULLY' <(tail -f "$file")
+  tail -f "$file" | grep  --quiet --line-regexp --fixed-strings --max-count=1 --regexp=' THE ANALYSIS HAS NOT BEEN COMPLETED' --regexp=' THE ANALYSIS HAS COMPLETED SUCCESSFULLY'
 }
 
 wait_for_abaqus "$JOB"
